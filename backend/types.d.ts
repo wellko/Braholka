b0853832c87d2ca9512adcd7251529c79fa02cc7
Kwon Valerii <wellko24@gmail.com>
2023-05-06 15:44:25 +0600

    added User models

--- backend/types.d.ts
+++ backend/types.d.ts
@@ -1,3 +1,11 @@
 export interface CategoryType {
   name: string;
 }
+
+export interface IUser {
+  username: string;
+  password: string;
+  token: string;
+  displayName: string;
+  phoneNumber: string;
+}
