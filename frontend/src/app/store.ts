97efd4e932c4ec74656a44edea0f460b25930136
Kwon Valerii <wellko24@gmail.com>
2023-05-11 19:07:14 +0600

    created store and hooks for frontend app

---
+++ frontend/src/app/store.ts
@@ -0,0 +1,30 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist/es/constants';
+import storage from 'redux-persist/lib/storage';
+import { usersReducer } from '../features/users/UsersSlice';
+
+const usersPersistConfig = {
+  key: 'nanafo:users',
+  storage,
+  whitelist: ['user'],
+};
+
+const rootReducer = combineReducers({
+  users: persistReducer(usersPersistConfig, usersReducer),
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
