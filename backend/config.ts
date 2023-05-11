cf9d61420a9f2d769a475c5770c44562fb10ef1f
Kwon Valerii <wellko24@gmail.com>
2023-05-11 19:05:45 +0600

    changed name of db and added google env

--- backend/config.ts
+++ backend/config.ts
@@ -5,7 +5,11 @@ const rootPath = __dirname;
 const config = {
   rootPath,
   publicPath: path.join(rootPath, 'public'),
-  db: 'mongodb://localhost/baraholka',
+  db: 'mongodb://localhost/nanafo',
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  },
 };
 
 export default config;
