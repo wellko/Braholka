fcf7af5c951c88fc4a4ebd869a54e9c0726b0a3a
Kwon Valerii <wellko24@gmail.com>
2023-05-11 19:09:58 +0600

    added persistor and all providers

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { CircularProgress } from '@mui/material';
+import { persistor, store } from './app/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { GOOGLE_CLIENT_ID } from './constants';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Suspense fallback={<CircularProgress />}>
+            <App />
+          </Suspense>
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </GoogleOAuthProvider>,
+);
