6922114281d93b225b5b8f095072a61883343233
Kwon Valerii <wellko24@gmail.com>
2023-05-17 11:12:04 +0600

    added logo image and css rules

--- frontend/src/components/UI/AppToolBar/AppToolBar.tsx
+++ frontend/src/components/UI/AppToolBar/AppToolBar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { AppBar, Grid, styled, Toolbar, Typography } from '@mui/material';
+import { AppBar, Grid, styled, Toolbar } from '@mui/material';
 import { Link as NavLink } from 'react-router-dom';
 import { useAppSelector } from '../../../app/hooks';
 import UserMenu from './UserMenu';
 import AnonymousMenu from './AnonymousMenu';
 import { selectUser } from '../../../features/users/UsersSlice';
+import logo from '../../../public/logo.png';
 
 const Link = styled(NavLink)({
   color: 'inherit',
@@ -21,10 +22,12 @@ const AppToolbar = () => {
     <AppBar position="sticky" sx={{ mb: 2, bgcolor: '#8707ff', boxShadow: 'inset 0px 0px 10px #FFF' }}>
       <Toolbar>
         <Grid container justifyContent="space-between" alignItems="center">
-          <Typography variant="h6" component="div">
-            <Link to="/">nanafo</Link>
-          </Typography>
-          <Grid item>{user ? <UserMenu user={user} /> : <AnonymousMenu />}</Grid>
+          <Link to="/">
+            <img className="logo" src={logo} alt="logo" />
+          </Link>
+          <Grid container item xs={12} sm={3}>
+            {user ? <UserMenu user={user} /> : <AnonymousMenu />}
+          </Grid>
         </Grid>
       </Toolbar>
     </AppBar>
