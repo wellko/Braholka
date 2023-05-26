623981042104cccf8e4938c1f1554ea1752db166
Kwon Valerii <wellko24@gmail.com>
2023-05-26 10:51:35 +0600

    added new router for ws chat

--- backend/index.ts
+++ backend/index.ts
@@ -5,10 +5,18 @@ import config from './config';
 import usersRouter from './routers/Users';
 import CategoriesRouter from './routers/Categories';
 import DealsRouter from './routers/Deals';
+import expressWs from 'express-ws';
+import { ActiveConnections, dealType, IncomingMessage, MessageType, WhisperType } from './types';
+import Message from './models/Message';
+import User from './models/User';
+import Deal from './models/Deal';
 
 const app = express();
 app.use(express.static('public'));
 app.use(cors());
+expressWs(app);
+const router = express.Router();
+const activeConnections: ActiveConnections = {};
 const port = 8000;
 app.use(express.json());
 app.use('/users', usersRouter);
@@ -24,5 +32,88 @@ const run = async () => {
     mongoose.disconnect();
   });
 };
+
+router.ws('/chat/:room', async (ws, req) => {
+  const { room } = req.params;
+  const id = req.body.id;
+  if (!activeConnections[room]) {
+    activeConnections[room] = {};
+  }
+  activeConnections[room][id] = ws;
+  const messages = await Message.find({ room }).sort({ _id: -1 }).limit(30).populate('author', 'displayName');
+  ws.send(
+    JSON.stringify({
+      type: 'EXISTING_MESSAGES',
+      payload: messages,
+    }),
+  );
+  ws.on('close', () => {
+    console.log(`Client disconnected! id=${id}, room=${room}`);
+    delete activeConnections[room][id];
+  });
+  ws.on('message', async (msg) => {
+    const decodedMessage = JSON.parse(msg.toString()) as IncomingMessage;
+    switch (decodedMessage.type) {
+      case 'SEND_NUMBER':
+        try {
+          const deal = (await Deal.findById(room)) as dealType;
+          const decodedMessage = JSON.parse(msg.toString()) as WhisperType;
+          if (JSON.stringify(deal.owner) === JSON.stringify(decodedMessage.author)) {
+            const whisperMessage = new Message({
+              author: decodedMessage.author,
+              text: decodedMessage.text,
+              room,
+              whisperTo: decodedMessage.to,
+              date: Date.now(),
+              whisper: true,
+            });
+            await whisperMessage.save();
+            ws.send(
+              JSON.stringify({
+                type: 'NEW_MESSAGE',
+                payload: {
+                  author: decodedMessage.author,
+                  to: decodedMessage.to,
+                  text: decodedMessage.text,
+                  date: Date.now(),
+                },
+              }),
+            );
+          }
+        } catch (error) {
+          ws.send(JSON.stringify(error));
+        }
+        break;
+      case 'SEND_MESSAGE':
+        try {
+          const messagePayload = decodedMessage.payload as MessageType;
+          const message = new Message({
+            date: Date.now(),
+            author: messagePayload.author,
+            text: messagePayload.text,
+            room,
+          });
+          await message.save();
+          const author = await User.findOne({ _id: messagePayload.author });
+          Object.keys(activeConnections[room]).forEach((connId) => {
+            const conn = activeConnections[room][connId];
+            conn.send(
+              JSON.stringify({
+                type: 'NEW_MESSAGE',
+                payload: {
+                  author: author,
+                  text: messagePayload.text,
+                  date: messagePayload.date,
+                },
+              }),
+            );
+          });
+        } catch (error) {
+          ws.send(JSON.stringify(error));
+        }
+        break;
+    }
+  });
+});
 
 run().catch(console.error);
