5aa42fc071bb7d4e267e57175491495d59a4c96b
Kwon Valerii <wellko24@gmail.com>
2023-05-26 10:50:10 +0600

    created new model for saving messages

---
+++ backend/models/Message.ts
@@ -0,0 +1,36 @@
+import mongoose, { Types } from 'mongoose';
+import User from './User';
+
+const Schema = mongoose.Schema;
+const MessageSchema = new Schema({
+  room: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => User.findById(value),
+      message: 'User not found!',
+    },
+    whisper: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+});
+
+const Message = mongoose.model('Message', MessageSchema);
+
+export default Message;
