0cdc87366567e2f61ed36fadab8733f9e483e5bd
Kwon Valerii <wellko24@gmail.com>
2023-05-30 22:01:25 +0600

    added new information and styles in dealpage

--- frontend/src/features/deals/DealPage.tsx
+++ frontend/src/features/deals/DealPage.tsx
@@ -5,14 +5,13 @@ import { getOneDeal } from './DealsThunks';
 import { selectDeal, selectDealsLoading } from './DealsSlice';
 import DotSpinner from '../../components/UI/DotSpinner/DotSpinner';
 import { apiUrl } from '../../constants';
-import { selectUser } from '../users/UsersSlice';
 import Chat from '../../components/UI/Chat/Chat';
-import { Grid } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Container, Grid } from '@mui/material';
+import QuestionMarkOutlinedIcon from '@mui/icons-material/QuestionMarkOutlined';
 
 const DealPage = () => {
   const { id } = useParams();
   const dispatch = useAppDispatch();
-  const user = useAppSelector(selectUser);
   const deal = useAppSelector(selectDeal);
   const loading = useAppSelector(selectDealsLoading);
   const imagePath = deal ? apiUrl + deal.image : undefined;
@@ -22,19 +21,50 @@ const DealPage = () => {
     }
   }, [id, dispatch]);
   return (
-    <Grid container>
-      {loading && <DotSpinner />}
-      {deal && (
-        <>
-          <h1 className="dealPage-header">{deal.title}</h1>
-          <div className="clearfix">
-            <img className="dealPage-image" src={imagePath} alt="product" />
-            <p>{deal.description}</p>
-          </div>
-          {user && <Chat />}
-        </>
-      )}
-    </Grid>
+    <Container>
+      <Grid container>
+        {loading && <DotSpinner />}
+        {deal && (
+          <>
+            <Grid item xs={12} md={6}>
+              <h2 className="dealPage-header">{deal.title}</h2>
+              <img src={imagePath} alt="product" className="dealPage-image" />
+              <Accordion>
+                <AccordionSummary expandIcon={<QuestionMarkOutlinedIcon />}>
+                  <p className="dealPage-accordion">
+                    состояние : <span className="dealPage-bold">{deal.condition} </span>
+                  </p>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <p className="dealPage-accordion">
+                    <span className="dealPage-bold">Новое</span> : вещь ниразу не использовали
+                  </p>
+                  <p className="dealPage-accordion">
+                    <span className="dealPage-bold">Идеальное</span> : вещь использовали несколько раз
+                  </p>
+                  <p className="dealPage-accordion">
+                    <span className="dealPage-bold">Очень хорошее</span> : вещь использовали более чем 5 раз, обращались
+                    очень бережно
+                  </p>
+                  <p className="dealPage-accordion">
+                    <span className="dealPage-bold">Удовлетворительное</span> : вещь использовали много раз
+                  </p>
+                </AccordionDetails>
+              </Accordion>
+              <p>{deal.description}</p>
+              {deal.purchasePrice > 0 ? (
+                <p>цена выкупа : {deal.purchasePrice} сом</p>
+              ) : (
+                <p>обмен на : {deal.tradeOn}</p>
+              )}
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Chat />
+            </Grid>
+          </>
+        )}
+      </Grid>
+    </Container>
   );
 };
 
